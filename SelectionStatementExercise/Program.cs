03bfebfb3d85521b547faf6d1d1e7e6cf905d92c
patrick smith <smithpatrick.510@gmail.com>
2023-06-21 15:31:50 -0400

    guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            //Number Guessing Game
+            var r = new Random();
+            var favNumber = r.Next(0, 1001);
+            int userInput;
+            var counter = 0;
+
+            Console.WriteLine("Pick a number from 1 - 1000");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+            }
+            else if (userInput > favNumber)
+            { 
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else 
+            {
+                Console.WriteLine("You've guessed it!");
+            }
+
+            counter++;
+
+            Console.WriteLine($"You have guessed {counter} times");
+
+            while (userInput != favNumber) ;
+        
+        }   
+         
+               
+            
     }
 }
